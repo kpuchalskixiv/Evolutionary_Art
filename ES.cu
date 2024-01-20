d3cb848bf2f4a6148f0f7d732cd7a1bb1dbe3c0a
kpuchalskixiv <k.puchalski.xiv@gmail.com>
2024-01-20 17:32:16 +0100

    added sigmas, sth is bugged

--- ES.cu
+++ ES.cu
@@ -32,10 +32,34 @@ float sum_array(float* arr, int start, int end){
   return sum;
 }
 
-void save_best(){
+void save_best(float* d_best_img, float* h_best_img,
+              int img_x, int img_y, int iter){
+  checkCudaErrors(cudaMemcpy(h_best_img, d_best_img, //destination, source
+                              img_x*img_y*sizeof(float),
+                              cudaMemcpyDeviceToHost) );
+  //save to file
+  FILE *fp;
+  if(iter==1) fp = fopen("./cuda_output/mona_results.txt", "w");
+  else fp = fopen("./cuda_output/mona_results.txt", "a");
+  // check for error here
 
+  fprintf(fp, "\n %d, ", iter);
+  for (unsigned i = 0; i < img_x*img_y; i++) {
+      fprintf(fp, "%f, ", h_best_img[i]);
+      // check for error here too
+  }
+
+  fclose(fp);
 }
 
+void read_target(float* target_img,  int img_x, int img_y){
+  FILE *fp;
+  fp=fopen("./input/mona_gray_cuda.txt", "r");
+  for(int i=0; i<img_x*img_y; i++){
+    fscanf(fp, "%f", &target_img[i]);
+  }
+  fclose(fp);
+}
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -44,16 +68,17 @@ void save_best(){
 
 int main(int argc, const char **argv){
 
-  int iters=20000, log_every=100;
-  bool gray=true;
+ // bool gray=true;
   int genotype_length;
-  int img_x=256, img_y=256;
+  int img_x=381, img_y=256;
 
   float *best_mate_img,
         *d_population,
         *d_population_copy,
         *d_population_images,
-        *d_mutation_coef,
+        *d_mutation_mates_coef, //*d_mutation_mates_if,
+        *d_sigmas, *d_sigmas_copy,
+        *d_mutation_sigmas_coef, *d_mutation_sigmas_if,
         *d_target_img, *h_target_img;
   
 
@@ -63,6 +88,7 @@ int main(int argc, const char **argv){
 
   size_t    bytes = (pop_size + children_per_mate*parents) *mate_size*genotype_length;
   size_t    eval_bytes =  (pop_size + children_per_mate*parents) *img_x*img_y;
+  size_t    sigmas_bytes =  (pop_size + children_per_mate*parents)*genotype_length;
 
   // grid is popsizeXcircler_per_mateXcricledim (x1,y1,x2,y2,alpha,gray - cause black&white for start)
   printf("Grid dimensions: %d x %d x %d \n\n", pop_size, mate_size, genotype_length);
@@ -72,11 +98,23 @@ int main(int argc, const char **argv){
   // allocate memory for arrays
   best_mate_img = (float *)malloc(sizeof(float) * img_y*img_x);
   h_target_img = (float *)malloc(sizeof(float) * img_y*img_x);
+
+  read_target(h_target_img, img_x, img_y);
+  printf("Target image read, sample pixel= %f\n", h_target_img[2137]);
+
   checkCudaErrors( cudaMalloc((void **)&d_population, sizeof(float) * bytes) );
+  checkCudaErrors( cudaMalloc((void **)&d_mutation_mates_coef, sizeof(float) * bytes) );
   checkCudaErrors( cudaMalloc((void **)&d_population_copy, sizeof(float) * bytes) );
   checkCudaErrors( cudaMalloc((void **)&d_population_images, sizeof(float) * eval_bytes) );
-  checkCudaErrors( cudaMalloc((void **)&d_mutation_coef, sizeof(float) *bytes) );
   checkCudaErrors( cudaMalloc((void **)&d_target_img, sizeof(float)  *img_x*img_y) );
+  
+  checkCudaErrors( cudaMalloc((void **)&d_mutation_sigmas_coef, sizeof(float) *sigmas_bytes) );
+  checkCudaErrors( cudaMalloc((void **)&d_mutation_sigmas_if, sizeof(float) *sigmas_bytes) );
+  checkCudaErrors( cudaMalloc((void **)&d_sigmas, sizeof(float)* sigmas_bytes) );
+  reset_values_kernel<<<pop_size+children_per_mate*parents, genotype_length>>>(d_sigmas, 0.5f, genotype_length);
+  getLastCudaError("Reset kernel failed\n");
+  checkCudaErrors( cudaMalloc((void **)&d_sigmas_copy, sizeof(float) *sigmas_bytes) );
+
 
   for(int i=0; i<img_x*img_y; i++) h_target_img[i]=(float)rand()/(float)(RAND_MAX);
   checkCudaErrors(cudaMemcpy(d_target_img, h_target_img, //destination, source
@@ -87,23 +125,28 @@ int main(int argc, const char **argv){
   
   curandGenerator_t gen;
   checkCudaErrors( curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT) );
-  checkCudaErrors( curandSetPseudoRandomGeneratorSeed(gen, 1234ULL) );
+ // checkCudaErrors( curandSetPseudoRandomGeneratorSeed(gen, 1234ULL) );
   checkCudaErrors( curandGenerateUniform(gen, d_population, bytes) );
-  checkCudaErrors( curandGenerateNormal(gen, d_mutation_coef, 
-                                        bytes,
-                                        1.0f, 0.001f) );
+
 
 
   // Set up the execution configuration
-  int no_circles=4, update_frequency=100, last_update_iter=0;
-  float *log_objective_values, *iter_evals;
+  int iters=100000, log_every=1000,
+
+  no_figures=1, update_frequency=5000;
+
+  
+  int last_update_iter=0;
+  float *log_objective_values;
   float add_cricle_threshold=0.01, prev, curr;
   log_objective_values=(float *)malloc(sizeof(float)*iters);
-
   int iter;
-  for (iter=1; iter<=iters; iter++) {
-    if (iter%log_every==0){
-      save_best();
+
+  for (iter=0; iter<=iters; iter++) {
+    if (iter%log_every==1){
+      save_best(d_population_images+population_losses[0].index*img_x*img_y, 
+                best_mate_img, 
+                img_x, img_y, iter);
       printf("iter number: %d. Best MSE: %f \n", iter, population_losses[0].value);
  /*    printf("%f %d, %f %d, %f %d \n", 
       population_losses[0].value, population_losses[0].index,
@@ -117,32 +160,51 @@ int main(int argc, const char **argv){
     checkCudaErrors(cudaMemcpy(d_population_copy, d_population, //destination, source
                             bytes*sizeof(float),
                             cudaMemcpyDeviceToDevice) );
+    checkCudaErrors(cudaMemcpy(d_sigmas_copy, d_sigmas, //destination, source
+                        sigmas_bytes*sizeof(float),
+                        cudaMemcpyDeviceToDevice) );
     cudaDeviceSynchronize();  
-    selection_kernel<<<parents, genotype_length*mate_size>>>(d_population, d_population_copy, genotype_length);
-    getLastCudaError("Selection kernel failed\n");
+    population_selection_kernel<<<parents, genotype_length*mate_size>>>(d_population, d_population_copy, genotype_length);
+    getLastCudaError("Population selection kernel failed\n");
+    population_selection_kernel<<<parents, genotype_length>>>(d_sigmas, d_sigmas_copy, genotype_length);
+    getLastCudaError("Sigmas selection kernel failed\n");
     cudaDeviceSynchronize();  
     checkCudaErrors(cudaMemcpy(d_population, d_population_copy, //destination, source
                             bytes*sizeof(float),
                             cudaMemcpyDeviceToDevice) );
+    checkCudaErrors(cudaMemcpy(d_sigmas, d_sigmas_copy, //destination, source
+                            sigmas_bytes*sizeof(float),
+                            cudaMemcpyDeviceToDevice) );
     cudaDeviceSynchronize();  
 
     //run mutation
-    checkCudaErrors( curandGenerateNormal(gen, d_mutation_coef, 
-                                    (pop_size+children_per_mate*parents) * mate_size * genotype_length,
-                                    1.0f, 0.001f) );
+    checkCudaErrors( curandGenerateNormal(gen, d_mutation_sigmas_coef, 
+                                    sigmas_bytes,
+                                    0.0f, 0.001f) );
+    checkCudaErrors( curandGenerateUniform(gen, d_mutation_sigmas_if, sigmas_bytes));
+    checkCudaErrors( curandGenerateUniform(gen, d_mutation_mates_coef, bytes));
     cudaDeviceSynchronize();
 
-    mutation_kernel<<<pop_size+children_per_mate*parents, genotype_length*mate_size>>>(d_population, d_mutation_coef, genotype_length);
+
+    sigmas_mutation_kernel<<<1, sigmas_bytes>>>(d_sigmas,  d_mutation_sigmas_coef, d_mutation_sigmas_if);
+    cudaDeviceSynchronize();
+    mate_mutation_kernel<<<pop_size+children_per_mate*parents, genotype_length*no_figures>>>(d_population, d_mutation_mates_coef, d_sigmas, genotype_length);
     getLastCudaError("Mutation kernel failed\n");
     cudaDeviceSynchronize();    
 
 
+
+
+
     //run evaluation
-    reset_images_kernel<<<pop_size+children_per_mate*parents, 512>>>(d_population_images, img_x, img_y);
+    //mem set doesnt work with floats
+   // checkCudaErrors(cudaMemset(d_population_images, 0.0f, sizeof(float)*img_x*img_y*(pop_size+children_per_mate*parents)));
+
+    reset_values_kernel<<<pop_size+children_per_mate*parents, 512>>>(d_population_images, 0.0f, img_x*img_y);
     getLastCudaError("Reset kernel failed\n");
     cudaDeviceSynchronize();
 
-    draw_kernel<<<pop_size+children_per_mate*parents, 512>>>(d_population, d_population_images, mate_size, img_x, img_y, genotype_length);
+    draw_kernel<<<pop_size+children_per_mate*parents, 512>>>(d_population, d_population_images, no_figures, img_x, img_y, genotype_length);
     getLastCudaError("Draw kernel failed\n");
     cudaDeviceSynchronize();
 
@@ -154,15 +216,17 @@ int main(int argc, const char **argv){
     qsort(population_losses, pop_size+children_per_mate*parents, sizeof(population_losses[0]), cmp);
 
 
-
+    log_objective_values[iter]=population_losses[0].value;
     if(iter%update_frequency==0 
       && iter-last_update_iter>=update_frequency*2 
-      && no_circles<=mate_size){
+      && no_figures<=mate_size){
       prev=sum_array(log_objective_values, iter-2*update_frequency, iter-update_frequency);
       curr=sum_array(log_objective_values, iter-update_frequency, iter);
 
-      if((prev-curr)/prev < add_cricle_threshold*powf(0.995, no_circles)){
-        no_circles+=1;
+      if((prev-curr)/prev < add_cricle_threshold*powf(0.995, no_figures)){
+        last_update_iter=iter;
+        no_figures+=1;
+        printf("Added rectangle at iter %d, current # is %d\n", iter, no_figures);
       }
       
     }
@@ -172,7 +236,7 @@ int main(int argc, const char **argv){
 
   checkCudaErrors( cudaFree(d_population) );
   checkCudaErrors( cudaFree(d_population_images) );
-  checkCudaErrors( cudaFree(d_mutation_coef) );
+  checkCudaErrors( cudaFree(d_mutation_sigmas_coef) );
 
   free(best_mate_img);
   free(log_objective_values);
--- ES_kernel.h
+++ ES_kernel.h
@@ -15,7 +15,9 @@ __device__ __managed__ loss_item population_losses[pop_size+children_per_mate*pa
 
 // block as parent, thread for each gene (blockDim.x=mate_sizeXgenotype_len threads)
 // select parent (blockidx.x), create children
-__global__ void selection_kernel(  float* population, float* population_copy, int genotype_length=8)
+__global__ void population_selection_kernel(  
+  float* population, float* population_copy, 
+  int genotype_length=8)
 {
   int parent_id = population_losses[blockIdx.x].index;
   int child_chromosome_id = (pop_size+blockIdx.x)*blockDim.x;
@@ -30,15 +32,46 @@ __global__ void selection_kernel(  float* population, float* population_copy, in
     child_chromosome_id+=blockDim.x;
   }
 }
+// block as parent, thread for each gene (blockDim.x=genotype_len threads)
+__global__ void sigmas_selection_kernel(  
+  float* sigmas, float* sigmas_copy, 
+  int genotype_length=8)
+{
+  int parent_id = population_losses[blockIdx.x].index;
+  int child_sigma_id = (pop_size+blockIdx.x)*blockDim.x;
+  int parent_sigma_id=parent_id*blockDim.x;
 
+  int tid=threadIdx.x;
+  // copy parent itself
+  sigmas_copy[blockIdx.x*mate_size*genotype_length+tid]=sigmas[parent_sigma_id+tid];
+  // create childrens, copies of parent
+  for(int i=0; i<children_per_mate; i++){
+    sigmas_copy[child_sigma_id+tid]=sigmas[parent_sigma_id+tid];
+    child_sigma_id+=blockDim.x;
+  }
+}
 
-
-// TODO !!!!
-// slowly unlock more circles as iterations progres
-__global__ void mutation_kernel(  float* populaion, float* mutation_coefs, int genotype_length=8)
+//<<<population, genotype_len>>>
+__global__ void sigmas_mutation_kernel(  
+  float* sigmas, 
+  float* mutation_coefs, float* mutation_ifs,
+  float scale=0.001f, float mut_prob=0.5f)
 {
   int tid = blockDim.x*blockIdx.x + threadIdx.x;
-  populaion[tid]*=mutation_coefs[tid];
+  if(mutation_ifs[tid]<mut_prob){
+    sigmas[tid]+=mutation_coefs[tid];
+  }
+}
+
+// <<<population, no_figures*genotype_len>>>
+__global__ void mate_mutation_kernel(  float* population, float* mutation_coefs, float* sigmas, int genotype_len)
+{
+  int tid = mate_size*blockIdx.x + threadIdx.x;
+
+
+  population[tid]+=sigmas[blockIdx.x*genotype_len + (threadIdx.x % genotype_len)]*(mutation_coefs[tid]-0.5f);
+  // int(lower_bound)=0
+  population[tid]=fmaxf(0.002f, fminf(population[tid], 1.0f));
 }
 
 // eval_kernel <<<#mates, 1024, shared_mem>>
@@ -82,11 +115,11 @@ __global__ void eval_kernel(
 }
 
 // block per image
-__global__ void reset_images_kernel(float* population_images, int x_img, int y_img){
-  int img_start=blockIdx.x*x_img*y_img;
+__global__ void reset_values_kernel(float* array, float value, int size){
+  int arr_start=blockIdx.x*size;
   int pixd=threadIdx.x;
-  while(pixd<x_img*y_img){
-    population_images[img_start+pixd]=0.0f;
+  while(pixd<size){
+    array[arr_start+pixd]=value;
     pixd+=blockDim.x;
   }
 
@@ -107,44 +140,58 @@ __global__ void draw_kernel(
   mate_idx=blockIdx.x;
   mate_chromosome_idx=mate_idx*mate_size*genotype_length;
   
-  int x_ul,y_ul,x_br,y_br, x_size, y_size, pixd, pixd_row, pixd_col, rectangle_start, org_pixel;
+  int x_ul,y_ul,x_br,y_br, x_size, y_size, pixd, pixd_row, pixd_col, rectangle_start;
   float opacity, g;
   for(int r=0; r<limit; r++){ //r for recatangle
-    x_ul=(int)(population[mate_chromosome_idx]*(img_x-1));
-    y_ul=(int)(population[mate_chromosome_idx+1]*(img_y-1));
-    x_br=(int)(population[mate_chromosome_idx+2]*(img_x-1));
-    y_br=(int)(population[mate_chromosome_idx+3]*(img_y-1));
+    x_ul=(int)(population[mate_chromosome_idx]*(img_x));
+    y_ul=(int)(population[mate_chromosome_idx+1]*(img_y));
+    x_br=(int)(population[mate_chromosome_idx+2]*(img_x));
+    y_br=(int)(population[mate_chromosome_idx+3]*(img_y));
+
+/*   x_ul=population[mate_chromosome_idx];
+    y_ul=population[mate_chromosome_idx+1];
+    x_br=population[mate_chromosome_idx+2];
+    y_br=population[mate_chromosome_idx+3];
+
+    if(x_ul>x_br){
+      population[mate_chromosome_idx]=x_br;
+      population[mate_chromosome_idx+2]=x_ul;
+    }if(y_ul>y_br){
+      population[mate_chromosome_idx+1]=y_br;
+      population[mate_chromosome_idx+3]=y_ul;
+    }*/
+
     opacity=population[mate_chromosome_idx+4];
     g=population[mate_chromosome_idx+5];
-
-    x_ul=min(max(0, x_ul), img_x);
-    y_ul=min(max(0, y_ul), img_y);
-    x_br=min(max(0, x_br), img_x);
-    y_br=min(max(0, y_br), img_y);
+  //  __syncthreads(); 
+    x_ul=min(max(0, x_ul), img_x-1);
+    y_ul=min(max(0, y_ul), img_y-1);
+    x_br=min(max(0, x_br), img_x-1);
+    y_br=min(max(0, y_br), img_y-1);
 
     x_size=max(0, x_br-x_ul);
     y_size=max(0, y_br-y_ul);
 
     pixd=threadIdx.x;
     pixd_row=pixd/y_size;
-    pixd_col=pixd%x_size;
+    pixd_col=pixd%y_size;
 
     rectangle_start=mate_idx*img_x*img_y // idx of mate image
                         +x_ul*img_y + y_ul ; // start of rectangle
-
+    __syncthreads(); 
     while((pixd<x_size*y_size)
-    && (pixd_row<y_size) 
-    && (pixd_col<x_size)){ // cache misses should occur whilst changing rows
+    && (pixd_row<x_size) 
+    && (pixd_col<y_size)){ // cache misses should occur whilst changing rows
       population_images[rectangle_start
-                        +pixd_row*img_y + y_ul+pixd_col
+                        +pixd_row*img_y +pixd_col
                         ]*=(1-opacity);
       population_images[rectangle_start
-                        +pixd_row*img_y + y_ul+pixd_col
+                        +pixd_row*img_y +pixd_col
                         ]+=opacity*g;
       
       pixd+=blockDim.x;
       pixd_row=pixd/y_size;
-      pixd_col=pixd%x_size;
+      pixd_col=pixd%y_size;
      // __syncthreads(); 
     }
     //ensure previous rectangle has been drawn
